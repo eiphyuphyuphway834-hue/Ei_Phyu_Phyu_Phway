0102e8c5131a750e551ee56f141cd6cf91fcb8fa
eiphyuphyuphway834-hue <eiphyuphyuphway834@gmail.com>
2026-06-30 11:58:32 +0000

    Week 6 complete

---
+++ w_6_java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Lecturer lecturer = new Lecturer("L100", "Dr Ahmad", "OOP");
+
+        lecturer.displayInfo();
+        lecturer.displaySubject();
+    }
+}
